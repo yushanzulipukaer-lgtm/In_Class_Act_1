68bf35d4c8823429736944eb714012ef52454a41
Zack <zulpikar628@gmail.com>
2026-09-25 11:25:32 -0400

    Update Singleton.cs

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -6,7 +6,7 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance { get; private set; }
 
-    protected virtual void Awake()
+    protected void Awake()
     {
         if(Instance != null && Instance!= this)
         {
